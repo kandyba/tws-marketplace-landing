f8a230ebf279d5dbb3d94f6703a6096b43b62ed2
Serhii Kandyba <seregakandyba@gmail.com>
2020-04-21 15:13:03 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp           = require('gulp'),
+	scss           = require('gulp-sass'),
+	browserSync    = require('browser-sync'),
+	concat         = require('gulp-concat'),
+	uglify         = require('gulp-uglify'),
+	cleanCSS       = require('gulp-clean-css'),
+	rename         = require('gulp-rename'),
+	autoprefixer   = require('gulp-autoprefixer'),
+	notify         = require("gulp-notify");
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false,
+	});
+});
+
+gulp.task('js', function() {
+	return gulp.src([
+		'app/libs/jquery/dist/jquery.min.js',
+		'app/js/common.js',
+		])
+	.pipe(concat('scripts.min.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('app/js'))
+	.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('scss', function() {
+	return gulp.src('app/scss/**/*.scss')
+	.pipe(scss({outputStyle: 'expand'}).on("error", notify.onError()))
+	.pipe(rename({suffix: '.min', prefix : ''}))
+	.pipe(autoprefixer(['last 15 versions']))
+	.pipe(cleanCSS())
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('watch', ['scss', 'js', 'browser-sync'], function() {
+	gulp.watch('app/scss/**/*.scss', ['scss']);
+	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['js']);
+	gulp.watch('app/*.html', browserSync.reload);
+});
+
+gulp.task('default', ['watch']);
